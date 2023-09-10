2e18acea84db6e9cc38707011e9402bb0996a5b3
Vladislav Zagoronyuk <vlad.v.zag@gmail.com>
2023-09-10 20:21:05 +0300

    01-HWC-cache: tests and user interface added, cache algorithm tested

--- 01-HWC-cache/Source/cache.cpp
+++ 01-HWC-cache/Source/cache.cpp
@@ -1,10 +1,28 @@
-#ifndef CACHE_CPP
-#define CACHE_CPP
-
 #include <iostream>
-#include <unordered_map>
-#include <iterator>
-#include <list>
 #include "../Include/cache.hpp"
 
-#endif
+int main()
+{
+    size_t correct_tests = 0;
+    size_t cache_size    = 0;
+
+    std::cin >> cache_size;
+    Cache_t<int> cache(cache_size);
+
+    size_t n_keys;
+    std::cin >> n_keys;
+
+    int key     = 0;
+    size_t hits = 0;
+
+    while (n_keys--)
+    {
+        std::cin >> key;
+        if (cache.update(key)) ++hits;
+        // cache.dump();
+    }
+
+    std::cout << hits << "\n";
+
+    return 0;
+}
--- 01-HWC-cache/Test/test.cpp
+++ 01-HWC-cache/Test/test.cpp
@@ -1,61 +1,50 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <new>
-#include <climits>
 #include <fstream>
-#include <sstream>
+#include <cassert>
 #include "../Include/cache.hpp"
-
-void test_file(std::string filename)
-{
-    std::ifstream test_data(filename);
-
-    if (test_data.is_open())
-    {
-        size_t test_number = 0;
-        size_t correct_tests = 0;
-        size_t cache_size;
-
-        while (test_data >> cache_size)
-        {
-            Cache_t<int> cache(cache_size);
-
-            std::cout << "\n" << ">>> TEST #" << ++test_number << "\n\n";
-
-            size_t number_of_keys;
-            test_data >> number_of_keys;
-
-            int key;
-            size_t hits = 0;
-
-            while (number_of_keys-- != 0)
-            {
-                test_data >> key;
-                if (cache.update(key)) ++hits;
-                cache.dump();
-            }
-
-            size_t result;
-            test_data >> result;
-
-            if (hits == result)
-            {
-                std::cout << ">>> SUCCESS\n";
-                ++correct_tests;
-            }
-            else
-                std::cout << ">>> ERROR: expected " << result << ", recieved" << hits << "\n";
-        }
-
-        std::cout << "\n========================================================= \n\n"
-              << ">>> CORRECT TESTS: " << correct_tests << " / " << test_number << "\n\n";
-    }
-}
 
 int main()
 {
-    test_file("test_data.txt");
+    std::ifstream test_data("test_data.txt");
+    assert(test_data.good());
+
+    size_t test_number = 0;
+    size_t correct_tests = 0;
+    size_t cache_size = 0;
+
+    while (test_data >> cache_size)
+    {
+        Cache_t<int> cache(cache_size);
+
+        std::cout << "\n" << ">>> TEST #" << ++test_number << "\n\n";
+
+        size_t n_keys;
+        test_data >> n_keys;
+
+        int key;
+        size_t hits = 0;
+
+        while (n_keys--)
+        {
+            test_data >> key;
+            if (cache.update(key)) ++hits;
+            // cache.dump();
+        }
+
+        size_t result;
+        test_data >> result;
+
+        if (hits == result)
+        {
+            std::cout << ">>> SUCCESS\n";
+            ++correct_tests;
+        }
+        else
+            std::cout << ">>> ERROR: expected " << result << ", recieved" << hits << "\n";
+    }
+
+    std::cout << "\n========================================================= \n\n"
+            << ">>> CORRECT TESTS: " << correct_tests << " / " << test_number << "\n\n";
 
     return 0;
 }
